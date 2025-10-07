6419e9356371a820e912425c06fb55a2379bd7e4
Hamilton Tumenas Borges <hamiltontubo@gmail.com>
2025-10-07 13:11:41 -0300

    inital commit

---
+++ src/main/java/com/br/h6n/bff_agendador/BffAgendadorApplication.java
@@ -0,0 +1,13 @@
+package com.br.h6n.bff_agendador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffAgendadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffAgendadorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/h6n/bff_agendador/BffAgendadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.h6n.bff_agendador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffAgendadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
